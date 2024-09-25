3a1d1a5184ece80e9bb94dbfcd92105d133079ac
Rehan Hussain <rehanhussain1991@gmail.com>
2024-09-25 12:38:12 +0500

    Project Setup

---
+++ Threads_IOS_App/Threads_IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_IOS_AppApp.swift
+//  Threads_IOS_App
+//
+//  Created by Mac on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Threads_IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
